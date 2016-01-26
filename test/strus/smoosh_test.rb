725a4d1a591bc36545565f6870d9623a96252dd1
Dave Strus <dave@getfretless.com>
2016-01-26 15:15:39 -0500

    Add failing test for Array#smoosh.

--- test/strus/smoosh_test.rb
+++ test/strus/smoosh_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 describe Strus::Smoosh do
   before do
     @test_string = " I <3 ruby\n\nyes I do    "
+    @test_array = [@test_string, " w00t\n w00t ", "\t\they he\ny"]
   end
 
   it 'has a version number' do
@@ -15,5 +16,9 @@ describe Strus::Smoosh do
 
   it 'smooshes the content of a string' do
     @test_string.smoosh.must_equal 'I<3rubyyesIdo'
+  end
+
+  it 'smooshes each string in an array and joins them' do
+    @test_array.smoosh.must_equal 'I<3rubyyesIdow00tw00theyhey'
   end
 end
