cf3cfe97b749f10f7d8cdc4ad59f4c4426dd4078
Dave Strus <dave@getfretless.com>
2016-01-26 15:07:34 -0500

    Remove all whitespace from strings.

--- lib/strus/smoosh.rb
+++ lib/strus/smoosh.rb
@@ -2,6 +2,11 @@ require "strus/smoosh/version"
 
 module Strus
   module Smoosh
-    # Your code goes here...
+    def smoosh
+      self.split.join
+    end
   end
 end
+
+# String.include(Strus::Smoosh)
+String.send(:include, Strus::Smoosh)
